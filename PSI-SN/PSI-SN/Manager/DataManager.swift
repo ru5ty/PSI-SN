7b54d35d660996191b0c39fff93abe4ec742c29d
Agus Rustandi <agus.rustandi@kiranatama.com>
2018-03-30 02:31:43 +0700

    compose pollutant data to model

--- PSI-SN/PSI-SN/Manager/DataManager.swift
+++ PSI-SN/PSI-SN/Manager/DataManager.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class DataManager: NSObject {
+    static let sharedInstance = DataManager()
+    
+    func composePollutantData(data: [String : Any]) -> [Region]! {
+        var regions = [Region]()
+        for region : [String : Any] in data["region_metadata"] as! [[String : Any]] {
+            let newRegion = Region.init(region)
+            let firstItem = (data["items"] as! [[String : Any]]).first
+            newRegion.initReading(firstItem!["readings"] as! [String : Any])
+            regions.append(newRegion)
+        }
+        return regions
+    }
+}
