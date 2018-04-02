b3e805fc66a49c5a6b8f663479c98ffdeee0f8af
Agus Rustandi <agus.rustandi@kiranatama.com>
2018-04-02 14:02:57 +0700

    created extention class created detail vc

---
+++ PSI-SN/PSI-SN/Utilities/ExtensionClass.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionClass.swift
+//  PSI-SN
+//
+//  Created by Agus Rustandi on 01/04/18.
+//  Copyright © 2018 Sample. All rights reserved.
+//
+
+import Foundation
---
+++ PSI-SN/PSI-SN/View Controller/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  PSI-SN
+//
+//  Created by Agus Rustandi on 01/04/18.
+//  Copyright © 2018 Sample. All rights reserved.
+//
+
+import Foundation
