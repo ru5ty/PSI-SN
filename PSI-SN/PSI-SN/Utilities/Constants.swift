0af7cd9e474810162a296f214aee8d1b1f2bb405
Agus Rustandi <agus.rustandi@kiranatama.com>
2018-04-03 05:32:10 +0700

    added enum input type

--- PSI-SN/PSI-SN/Utilities/Constants.swift
+++ PSI-SN/PSI-SN/Utilities/Constants.swift
@@ -12,4 +12,25 @@ let kMAPS_API = "AIzaSyDCZpGVZ78JqfqrSlJGeCC5Mn7lZbXW8EE"
 
 struct Constants {
     
+    /**
+     Struct usable enumeration
+     */
+    struct Enumeration {
+        /**
+         Input picker options.
+         ````
+         case Date
+         case Time
+         case Other
+         ````
+         */
+        enum InputType : Int, EnumCollection {
+            /// Input picker with Date type
+            case Date = 1
+            /// Input picker with Time type
+            case Time = 2
+            /// Input picker with Custom type
+            case Other = 3
+        }
+    }
 }
